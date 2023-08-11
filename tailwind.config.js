a041610f4ce7fcfa40e87a14f03ccd9068df26f1
itzmohand16 <137426396+itzmohand16@users.noreply.github.com>
2023-08-11 17:14:31 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        backgroundImage: {
+        },
+
+        theme: {
+          colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'purple': '#3f3cbb',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+            'fuck1': '#2c2425',
+            'fuck2': '#28272a',
+          },
+        }, 
+        
+        
+
+      extend: {},
+    },
+    plugins: [],
+  }
+  
